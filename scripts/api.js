01cd0dec61ff8de9481afc3dd386101cd6a29594
giovanni <mormagli@gmail.com>
2018-05-16 10:50:45 -0500

    filled out getItem method

--- scripts/api.js
+++ scripts/api.js
@@ -1,10 +1,15 @@
+'use strict';
+/*global $*/
+
 const api = (function() {
-    const BASE_URL = 'https://thinkful-list-api.herokuapp.com/alex';
-    const getItems = function (callback) {
-        callback('api module works!');
-    }
-    return {
-        getItems,
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/alex';
+  
+  const getItems = function (callback) {
+    $.getJSON(BASE_URL+'/items',callback);
+  };
+
+  return {
+    getItems,
         
-    };
+  };
 }());
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
 /* global shoppingList, store */
+'use strict';
+
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
